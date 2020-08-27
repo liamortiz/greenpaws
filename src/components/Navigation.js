4f3827e13bb3200456614a36afe923111738a4ba
Leamsi Escribano <leamsi.esc@gmail.com>
2020-08-27 14:22:15 -0400

    Working on finishing the navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,21 @@ import React from 'react';
 const Navigation = props => {
     return (
         <nav>
-            <h1 className = "logo">greenpaws <i className = "paws"></i></h1>
+            <h1 className = "logo">greenpaws <i className = "icon paws"></i></h1>
+            <ul className = "nav-items basic">
+                <li className = "expandable">Shop</li>
+                <li className = "expandable">Brands</li>
+                <li>On Sale</li>
+            </ul>
+            <form className="search-bar">
+                <div className="search-wrapper">
+                    <input type = "text" placeholder="Search our products"/>
+                    <button type = "submit"><i className= "icon search"></i></button>
+                </div>
+            </form>
+            <div className="nav-items right-side">
+                <i className="icon account"></i>
+            </div>
         </nav>
     )
 }
